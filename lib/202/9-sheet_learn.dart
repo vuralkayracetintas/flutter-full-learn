f52ddd30e222185c9cee6ace0f4f9ca2fb854f15
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-09-26 09:36:07 +0300

    sheet learn

--- lib/202/9-sheet_learn.dart
+++ lib/202/9-sheet_learn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/101/11-image_learn.dart';
 import 'package:kartal/kartal.dart';
 
 class SheetLearn extends StatefulWidget {
@@ -8,40 +9,183 @@ class SheetLearn extends StatefulWidget {
   State<SheetLearn> createState() => _SheetLearnState();
 }
 
-class _SheetLearnState extends State<SheetLearn> {
+class _SheetLearnState extends State<SheetLearn> with ProductSheetMixin {
+  Color _backgroundColor = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showModalBottomSheet(
+          final result = showModalBottomSheet(
               context: context,
-              backgroundColor: Colors.green,
+              backgroundColor: _backgroundColor,
               barrierColor: Colors.transparent,
               shape: const RoundedRectangleBorder(
                   borderRadius:
                       BorderRadius.vertical(top: Radius.circular(24))),
               builder: (context) {
-                return SizedBox(
-                  width: context.sized.height,
-                  child: Column(
-                    children: [
-                      const Text('data'),
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('data'))
-                    ],
-                  ),
-                );
+                return _CustomSheet(backgroundColor: _backgroundColor);
               });
+          if (result is bool) {
+            setState(() {
+              _backgroundColor = Colors.red;
+            });
+          }
         },
         child: const Icon(Icons.add),
       ),
-      body: const Center(
-        child: Text('text'),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            showCustomSheet(context, ImageLearn());
+          },
+          child: Text('text'),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomSheet extends StatefulWidget {
+  const _CustomSheet({
+    required Color backgroundColor,
+  });
+
+  @override
+  State<_CustomSheet> createState() => _CustomSheetState();
+}
+
+class _CustomSheetState extends State<_CustomSheet> {
+  Color _backgroundColor = Colors.white;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: _backgroundColor,
+      child: SafeArea(
+        child: Padding(
+          padding: context.padding.medium,
+          child: SizedBox(
+            width: context.sized.width,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 24,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: 0,
+                          right: 0,
+                          child: InkWell(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                                print('closed');
+                              },
+                              child: const Icon(
+                                Icons.close,
+                                color: Colors.black,
+                              ))),
+                      Divider(
+                        color: Colors.red,
+                        thickness: 4,
+                        indent: context.sized.width * 0.3,
+                        endIndent: context.sized.width * 0.3,
+                      ),
+                    ],
+                  ),
+                ),
+                Text('data',
+                    style: context.general.textTheme.headlineLarge?.copyWith(
+                      color: Colors.black,
+                    )),
+                Image.network(
+                  'https://picsum.photos/200/300',
+                  height: 200,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _backgroundColor = Colors.red;
+                      });
+                      Navigator.of(context).pop<bool>(true);
+                    },
+                    child: const Text('data'))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+mixin ProductSheetMixin {
+  Future showCustomSheet(BuildContext context, Widget child) async {
+    return showModalBottomSheet(
+        context: context,
+        barrierColor: Colors.transparent,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(24))),
+        builder: (context) {
+          return CustomMainSheet(child: child);
+        });
+  }
+}
+
+class CustomMainSheet extends StatelessWidget {
+  const CustomMainSheet({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: context.padding.medium,
+        child: SizedBox(
+          width: context.sized.width,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const _BaseSheetHeader(),
+              Expanded(child: child),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BaseSheetHeader extends StatelessWidget {
+  const _BaseSheetHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 24,
+      child: Stack(
+        children: [
+          Positioned(
+              top: 0,
+              right: 0,
+              child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    print('closed');
+                  },
+                  child: const Icon(
+                    Icons.close,
+                    color: Colors.black,
+                  ))),
+          Divider(
+            color: Colors.red,
+            thickness: 4,
+            indent: context.sized.width * 0.3,
+            endIndent: context.sized.width * 0.3,
+          ),
+        ],
       ),
     );
   }
